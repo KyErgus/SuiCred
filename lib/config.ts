7789285de31bc5c999525825c17ce45328c3821d
catone <catone@catone-MacBook-Air.local>
2026-02-06 00:17:00 +0300

    Set SuiCred package id

--- lib/config.ts
+++ lib/config.ts
@@ -2,7 +2,7 @@ export const TREASURY_ADDRESS =
   '0xce5c72750ddcbfbead5c3690c580a7835eab6dacfef98f36cb167fc9f351e87f';
 
 // TODO: Replace after publishing Move package
-export const SUICRED_PACKAGE_ID = '0xYOUR_PACKAGE_ID';
+export const SUICRED_PACKAGE_ID = '0x20e2efc2f475be3527f0b9934154a9f026a016309ebfff2c27ee34d02170444d';
 export const SUICRED_MODULE = 'suicred';
 export const SUICRED_MINT_FUNCTION = 'mint_score';
 
