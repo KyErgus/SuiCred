1a161822524eb5bf26338817149b537fb93fd84b
catone <catone@catone-MacBook-Air.local>
2026-02-07 11:47:29 +0300

    Update package id for NFT cards

--- lib/config.ts
+++ lib/config.ts
@@ -1,8 +1,8 @@
 export const TREASURY_ADDRESS =
-  '0x2efa15af9a998444667acef1385131f654f0215d1ec13ae4580fff33a8e99f15';
+  '0x82ebab744e4334e71d1f1e4d72768bb56455e1063d9c4d82e8193a034625901b';
 
 // TODO: Replace after publishing Move package
-export const SUICRED_PACKAGE_ID = '0x2efa15af9a998444667acef1385131f654f0215d1ec13ae4580fff33a8e99f15';
+export const SUICRED_PACKAGE_ID = '0x82ebab744e4334e71d1f1e4d72768bb56455e1063d9c4d82e8193a034625901b';
 export const SUICRED_MODULE = 'suicred';
 export const SUICRED_MINT_FUNCTION = 'mint_score';
 
